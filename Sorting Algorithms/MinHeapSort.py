9e5f062c9f643db3a0a0f42e0706a6efe9b2cad6
markwreinke <46796939+markwreinke@users.noreply.github.com>
2019-10-26 15:37:36 -0400

    Max MinHeapSort

--- Sorting Algorithms/MinHeapSort.py
+++ Sorting Algorithms/MinHeapSort.py
@@ -5,15 +5,15 @@ import math
 #   Then, it steps down the array from the end, echanging the first element(which is always the largest, due to being a max tree)
 #   with the index, until the index is at position 1, in which the array is sorted.
 #   Runs in O(n lg n) time.
-def HeapSort(array):
-    heapSize = BuildMaxHeap(array)
+def MinHeapSort(array):
+    heapSize = BuildMinHeap(array)
 
     for i in range(len(array) - 1, 0, -1):
         temp = array[0]
         array[0] = array[i]
         array[i] = temp
         heapSize -= 1
-        MaxHeapify(array, heapSize, 0)
+        MinHeapify(array, heapSize, 0)
 
 # Determines the index of the parent node of the input index.
 def parentNode(i):
@@ -28,35 +28,35 @@ def RightNode(i):
     return 2*i + 2
 
 # This function takes in the array, the heapsize to be considered, and the index of the end of the input heap. This method has O(lg n) runtime.
-def MaxHeapify(array, heapSize, index):
+def MinHeapify(array, heapSize, index):
     leftNode  = LeftNode(index)
     rightNode = RightNode(index)
-    # If the left node exists and is larger than the end node, than it is recorded as the largest node, else the end node is recorded as the largest node.
-    if leftNode <= heapSize and array[leftNode] > array[index]:
-        largest = leftNode
+    # If the left node exists and is smaller than the end node, than it is recorded as the smallest node, else the end node is recorded as the smallest node.
+    if leftNode <= heapSize and array[leftNode] < array[index]:
+        smallest = leftNode
     else:
-        largest = index
+        smallest = index
 
-    # If the right node exists and is larger than the node currently marked as the largest, then the right node is recorded as the largest node.
-    if rightNode <= heapSize  and array[rightNode] > array[largest]:
-        largest = rightNode
+    # If the right node exists and is smaller than the node currently marked as the smallest, then the right node is recorded as the smallest node.
+    if rightNode <= heapSize  and array[rightNode] < array[smallest]:
+        smallest = rightNode
 
-    #If the largest node isn't last node, then it is exchanged with the last one, and runs heapify on the remaining branch of the tree.
-    if largest != index:
+    #If the smallest node isn't last node, then it is exchanged with the last one, and runs heapify on the remaining branch of the tree.
+    if smallest != index:
         temp = array[index]
-        array[index] = array[largest]
-        array[largest] = temp
-        MaxHeapify(array, heapSize, largest)
+        array[index] = array[smallest]
+        array[smallest] = temp
+        MinHeapify(array, heapSize, smallest)
 
 # Determines the heapSize of the entire array, then runs MaxHeapify on the first n/2 nodes (since the last n/2 nodes are already 1 element heaps).
 #   This runs in O(n) time.
-def BuildMaxHeap(array):
+def BuildMinHeap(array):
     heapSize = len(array) -1
-    for i in range(math.floor(heapSize/2), -1, -1):
-        MaxHeapify(array, heapSize, i)
+    for i in range(0, math.floor(heapSize/2) + 1, 1):
+        MinHeapify(array, heapSize, i)
 
     return heapSize
 
-a = [4,1,3,0,16,9,0,19,8,7]
-HeapSort(a)
+a = [4,1,3,2,16,9,10,14,8,7]
+MinHeapSort(a)
 print(a)
